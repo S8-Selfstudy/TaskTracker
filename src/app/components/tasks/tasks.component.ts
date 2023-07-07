452ac50dfdce4a500cd57416406d818d439e9ce3
Luca Bulles <lucabulles@gmail.com>
2023-07-07 15:59:32 +0200

    added add task method

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -26,4 +26,8 @@ export class TasksComponent {
     this.taskService.updateTaskReminder(task).subscribe();
   }
 
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
+  }
+
 }
