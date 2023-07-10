e111b4efe0e32c3db60baf74da1a4940e4ab0670
Luca Bulles <lucabulles@gmail.com>
2023-07-10 16:34:07 +0200

    form will show based on showAddTask method

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
+import { Subscription } from 'rxjs';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-add-task',
@@ -11,6 +13,16 @@ export class AddTaskComponent {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask?: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+
+
+  }
 
   onSubmit() {
     if (!this.text) {
